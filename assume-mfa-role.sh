c63b0d1112c1883bfccc389232246a79b7bcb230
Michael Price <michael.price@capgemini.com>
2020-10-30 12:42:10 +0000

    Updated README and cleaned up script

--- assume-mfa-role.sh
+++ assume-mfa-role.sh
@@ -10,13 +10,13 @@
 set -e
 
 # IAM switch role to assume
-SWITCH_ROLE_ARN='arn:aws:iam::<account-number>:role/<name>'
+SWITCH_ROLE_ARN='arn:aws:iam::<account>:role/<role>'
 
 # AWS profile which holds the credentials of the root account (which we are switching from)
-export AWS_PROFILE='<profile>'
+export AWS_PROFILE='<profile-name>'
 
 # Session duration - must be <= the max allowed for the role
-SESSION_DURATION=7200   # 2 hours
+SESSION_DURATION=3600     # 1 hours
 
 # Retrieve the MFA token as a parameter
 MFA_TOKEN=${1?"Please enter the MFA_TOKEN. Usage: $0 <MFA_TOKEN>"}
@@ -27,7 +27,7 @@ unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
 # Set CLI defaults
-AWS_DEFAULT_REGION='eu-west-2'
+AWS_DEFAULT_REGION='eu-west-1'
 AWS_DEFAULT_OUTPUT='json'
 
 
@@ -37,7 +37,7 @@ USERNAME=$(cut -d '/' -f2 <<< "${IAM_USER_ARN}")
 MFA_SERIAL_ARN=$(aws iam list-mfa-devices --user-name "${USERNAME}" | jq -r '.MFADevices[0].SerialNumber')
 
 # Assume the switch role, passing the MFA token
-RESPONSE=$(aws sts assume-role --role-arn "${SWITCH_ROLE_ARN}" --role-session-name "${USERNAME}-switch-role-session" --duration-seconds "${SESSION_DURATION}" --serial-number "${MFA_SERIAL_ARN}" --token-code "${MFA_TOKEN}")
+RESPONSE=$(aws sts assume-role --role-arn "${SWITCH_ROLE_ARN}" --role-session-name "${USERNAME}-switch-role-session" --duration-seconds  "${SESSION_DURATION}" --serial-number "${MFA_SERIAL_ARN}" --token-code "${MFA_TOKEN}")
 AWS_ACCESS_KEY_ID=$(echo "${RESPONSE}" | jq -r '.Credentials.AccessKeyId')
 AWS_SECRET_ACCESS_KEY=$(echo "${RESPONSE}" | jq -r '.Credentials.SecretAccessKey')
 AWS_SESSION_TOKEN=$(echo "${RESPONSE}" | jq -r '.Credentials.SessionToken')
@@ -48,8 +48,12 @@ export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
 export AWS_DEFAULT_REGION
 
-# Echo out the credentials in case we are not dot sourcing the script directly
-echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
-echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
-echo "export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
-echo "export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION"
+unset RESPONSE
+unset MFA_SERIAL_ARN
+unset USERNAME
+unset IAM_USER_ARN
+unset AWS_DEFAULT_OUTPUT
+unset MFA_TOKEN
+unset SESSION_DURATION
+unset AWS_PROFILE
+unset SWITCH_ROLE_ARN
